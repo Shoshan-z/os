6ba368b462c041cbb7d682f75195933acb3acf5e
gil Zelikovitz <gil Zelikovitz>
2017-01-21 11:05:50 +0200

    handle client functino exist but tested only one connection

--- ex4/os_server.c
+++ ex4/os_server.c
@@ -7,6 +7,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
 
 #define URANDOM_PATH "/dev/urandom"
 #define BUFF_SIZE 4096
@@ -16,6 +21,11 @@
  didn't check the conditions for the keyfile
  */
 
+ 
+
+//global variables
+int done = 0; 
+int clients_cnt = 0; //delete afterwards
 
 int write_all_to_file(int fd, char* buffer, int size){
   int total = 0; 
@@ -61,15 +71,15 @@ void init_key_file(char* key_path, int key_len){
   
   key_fd = open(key_path, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU |S_IRWXG | S_IROTH); 
   if (key_fd < 0 ){
-    printf( "error opening the key file: %s\n", strerror(errno));
+		printf( "error opening the key file: %s\n", strerror(errno));
     exit(errno);
-   }
+  }
 
   random_fd = open(URANDOM_PATH, O_RDONLY); 
   if (random_fd < 0 ){
-    printf( "error opening %s: %s\n", URANDOM_PATH, strerror(errno));
+		printf( "error opening %s: %s\n", URANDOM_PATH, strerror(errno));
     exit(errno);
-   }
+  }
 
   while (bytes_read < key_len) {
 
@@ -114,14 +124,50 @@ void parse_args(int argc, char* argv[], short* port, char** key_path, int* key_l
 }
 
 
+void sigint_handler (int signum)
+{
+  printf("sigint recieved\n"); 
+  done =1; 
+}
+
+void handle_client(int connfd) {
+	char buffer[BUFF_SIZE] = {0}; 
+	int read_bytes = 0; 
+	int sent_bytes = 0; 
+	
+	clients_cnt++; 
+  printf("handling client %d\n", clients_cnt);   
+	
+	
+	//while not done? 
+	
+	do {
+		//(int fd, char* buffer, int size
+		read_bytes = read_all_from_file(connfd, buffer, sizeof(buffer)); 
+		
+		if (read_bytes !=0) {
+			sent_bytes = write_all_to_file(connfd, buffer, sizeof(buffer)); 
+		}
+		
+	} while (read_bytes !=0 ); 
+	
+	
+}
+
 int main(int argc, char* argv[]){
   short port = 0;
   char* key_path = NULL;
   int key_len = 0; 
   int success = 0; 
+  struct sockaddr_in serv_addr = {0};
+  struct sockaddr_in my_addr = {0};  //check if i need this 
+  struct sockaddr_in peer_addr = {0}; //check if i need this
+  int listenfd = 0, connfd = 0;
+  struct sigaction sigint_action;
+  int f = 0; 
   
   if (argc < 3 || argc > 4) {
-    printf("Usage: %s <PORT> <KEY> [KEYLEN]\n", argv[0]); 
+    printf("usage: %s <PORT> <KEY> [KEYLEN]\n", argv[0]); 
     return 0; 
   }
   
@@ -131,7 +177,72 @@ int main(int argc, char* argv[]){
     init_key_file(key_path, key_len); 
   }
   
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) {
+	printf("error in socket(): %s\n", strerror(errno)); 
+	exit(errno); 
+  }
+   
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = any local machine address
+  serv_addr.sin_port = htons(port);
+  
+  
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))){
+    printf("error in bind(): %s \n", strerror(errno));
+    exit(errno); 
+  }
+
+  if(listen(listenfd, 10)){
+    printf("error in listen: %s \n", strerror(errno));
+    exit(errno); 
+  }
+  
+  //assign pointer to our handler functions
+  sigint_action.sa_handler = sigint_handler; 
+  
+  //remove any special flags
+  sigint_action.sa_flags = 0;
+
+  if (sigaction (SIGINT, &sigint_action, NULL) != 0) {
+    printf("signal handle registration failed %s\n",strerror(errno));
+    exit(errno); 
+  }
+  
+  
+  while (!done) {
+	    
+		socklen_t addrsize = sizeof(struct sockaddr_in );
+
+     /* accpeting connection. can use NULL in 2nd and 3rd arguments
+    but we want to print the client socket details*/
+    connfd = accept(listenfd, (struct sockaddr*)&peer_addr, &addrsize);
+
+    if(connfd<0){
+	  printf("error in accept: %s\n", strerror(errno));
+      exit(errno); 
+		}
+	
+		f = fork();
+		if (f < 0) {
+			printf("fork failed: %s\n", strerror(errno));
+			exit(errno);
+		}
+	
+		if (f=0) { //inside son process
+			close(listenfd);
+			handle_client(connfd); 
+			
+		}
+		else { //inside parent process 
+			close(connfd); 
+		}
+	  
+  }
+  
  cleanup:
+  
+  
   
   return 0; 
 }
